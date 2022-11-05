24ac36a093221b48b94eeaba3c5beb3b1a815d72
Vladimir <zanvlad677@gmail.com>
2022-11-05 19:18:56 +0300

    fix brain_prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,16 +1,16 @@
 import random
-import math
 
 
 def rand_prime():
-    count = 0
     result = ()
     for _ in range(3):
+        count = 0
         randon_number = random.randint(0, 200)
-        for i in range(1, int(math.sqrt(randon_number))):
+        for i in range(1, randon_number // 2 + 2):
+            print(i, count)
             if randon_number % i == 0:
                 count += 1
-            if count > 1:
+            if count > 1 or randon_number == 0 or randon_number == 1:
                 correct_answer = 'no'
                 break
         else:
